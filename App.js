e7fbf065641057e2d7b1760b02536da623c193ff
samaramiranda <samaramiranda.13@hotmail.com>
2020-10-30 23:15:20 -0300

    Estilizando inputs

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
 
 export default function App() {
+  const [url, setUrl] = useState("")
+  const [name, setName] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>App React Native</Text>
-      <Text>Shorlink</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Short
+        <Text style={{ color: "#1076f7" }}>Link</Text>
+      </Text>
+      <TextInput
+        style={styles.urlInput}
+        onChangeText={(texto) => setUrl(texto)}
+        value={url}
+        placeholder="Digite a URL..."></TextInput>
+      <TextInput
+      style={styles.urlInput}
+      onChangeText={ (texto) => setName(texto)}
+      value={name}
+      placeholder="Nome personalizado"
+      ></TextInput>
     </View>
   );
 }
@@ -15,8 +28,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  title: {
+    color: "#21243d",
+    fontWeight: "bold",
+    fontSize: 40,
+    marginBottom: 20
+  },
+  urlInput: {
+    height: 50,
+    width: "80%",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: "#fafafa",
+    marginBottom: 20,
+    fontSize: 20,
+  }
+})
