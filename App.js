2a66b101868cef8c7a5cea95fe8a6044dc647440
samaramiranda <samaramiranda.13@hotmail.com>
2020-10-31 19:22:36 -0300

    Adicionando botão e link encurtado

--- App.js
+++ App.js
@@ -16,11 +16,20 @@ export default function App() {
         value={url}
         placeholder="Digite a URL..."></TextInput>
       <TextInput
-      style={styles.urlInput}
-      onChangeText={ (texto) => setName(texto)}
-      value={name}
-      placeholder="Nome personalizado"
-      ></TextInput>
+        style={styles.urlInput}
+        onChangeText={(texto) => setName(texto)}
+        value={name}
+        placeholder="Nome personalizado"
+      >
+      </TextInput>
+      <TouchableOpacity onPress={() => { }} style={styles.shortBtn}>
+        <Text style={{ color: "#FFF" }}>Encurtar</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.finalUrl}>
+        https://cuttlt.com/nomeUrl
+      </Text>
+
     </View>
   );
 }
@@ -46,5 +55,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fafafa",
     marginBottom: 20,
     fontSize: 20,
+  },
+  shortBtn: {
+    backgroundColor: "#ff7c7c",
+    borderRadius: 20,
+    height: 40,
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  finalUrl: {
+    height: 40,
+    width: "80%",
+    marginTop: 20,
+    fontSize: 20,
+    textAlign: "center"
   }
 })
